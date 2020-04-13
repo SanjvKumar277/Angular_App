ffab0b2aac3805721180066530db36b73f8ecec9
sanjiv kumar <sanjivkumar277@gmail.com>
2020-04-13 18:00:28 +0530

    Architecture setup

---
+++ Quick Application1/DAL/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿// =============================
+// Email: info@ebenmonney.com
+// www.ebenmonney.com/templates
+// =============================
+
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DAL.Repositories.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        public ProductRepository(DbContext context) : base(context)
+        { }
+
+
+
+
+        private ApplicationDbContext _appContext => (ApplicationDbContext)_context;
+
+        public IEnumerable<Product> GetAllProductData()
+        {
+            return _appContext.Products
+                .OrderBy(c => c.Name)
+                .ToList();
+        }
+    }
+}
---
+++ Quick Application1/Quick_Application1/Controllers/ProductController.cs
@@ -0,0 +1,72 @@
+﻿// =============================
+// Email: info@ebenmonney.com
+// www.ebenmonney.com/templates
+// =============================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DAL;
+using Quick_Application1.ViewModels;
+using AutoMapper;
+using Microsoft.Extensions.Logging;
+using Quick_Application1.Helpers;
+
+namespace Quick_Application1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+        private readonly IMapper _mapper;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger _logger;
+        private readonly IEmailSender _emailSender;
+
+
+        public ProductController(IMapper mapper, IUnitOfWork unitOfWork, ILogger<CustomerController> logger, IEmailSender emailSender)
+        {
+            _mapper = mapper;
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+            _emailSender = emailSender;
+        }
+
+
+        // GET: api/Product
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var allProducts = _unitOfWork.Products.GetAllProductData();
+            return Ok(_mapper.Map<IEnumerable<ProductViewModel>>(allProducts));
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
